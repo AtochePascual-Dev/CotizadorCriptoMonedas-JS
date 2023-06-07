25473108757cc17809b908bcb6631b63db76249b
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-06 16:55:28 -1000

    Updates

---
+++ README.md
@@ -0,0 +1,24 @@
+# COTIZADOR CRIPTOMONEDAS
+
+## Descripción
+
+Este proyecto consiste en una aplicación web que permite obtener cotizaciones de criptomonedas. Al cargar el documento, se obtienen las 10 criptomonedas más importantes desde una API y se agregan al select correspondiente. Al enviar el formulario, se valida la información seleccionada y se realiza una solicitud a otra API para obtener la cotización de la criptomoneda seleccionada. La cotización se muestra en pantalla junto con otros datos relevantes, como el precio más alto y más bajo del día, la variación en las últimas 24 horas y la última actualización. Además, se implementa una funcionalidad para mostrar una alerta en caso de que no se haya seleccionado una moneda o una criptomoneda. También se incluye un spinner de carga durante la obtención de la cotización.
+
+## Instrucciones de uso
+
+Para acceder a la página, simplemente abra el archivo index.html en su navegador web
+
+## Tecnologías utilizadas
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+## Contribución
+
+Si deseas contribuir al proyecto, por favor, haz un fork del repositorio y crea una rama con tus cambios. Luego, envía una solicitud de extracción para que podamos revisar tus cambios antes de fusionarlos en la rama principal.
+
+# preview
+
+![](https://i.imgur.com/RHU7CI7.png)
---
+++ js/app.js
@@ -0,0 +1,157 @@
+// * VARIABLES
+const formulario = document.querySelector('#formulario');
+const selectMoneda = document.querySelector('#moneda');
+const selectCriptomonedas = document.querySelector('#criptomonedas');
+const divResultado = document.querySelector('#resultado');
+
+
+
+// * FUNCIONES
+// * Obtiene las 10 criptomonedas mas importante y los añada el select
+const obtenerCriptomonedas = () => {
+  const URL = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD`;
+
+  fetch(URL)
+    .then(respuesta => respuesta.json())
+    .then(resultado => {
+      const criptomonedas = resultado.Data;
+      mostrarCriptoMonedas(criptomonedas);
+    })
+};
+
+
+
+// * Muestra las criptomonedas obtenidas en el select
+const mostrarCriptoMonedas = (criptomonedas) => {
+
+  //  Iteremos sobre las criptomonedas
+  criptomonedas.forEach(criptomoneda => {
+    const { CoinInfo } = criptomoneda;
+    const { Name, FullName } = CoinInfo;
+    const option = document.createElement('OPTION');
+
+    option.textContent = FullName;
+    option.value = Name;
+
+    selectCriptomonedas.appendChild(option);
+  });
+};
+
+
+
+// * Cotiza una criptomoneda
+const cotizarCriptomoneda = (event) => {
+  event.preventDefault();
+
+  // obtenemos los valores de los select
+  const moneda = selectMoneda.value;
+  const criptomoneda = selectCriptomonedas.value;
+
+  // Validamos la informacion
+  if ([moneda, criptomoneda].includes('')) {
+    mostrarAlerta('Seleccione una Moneda y Criptomoneda');
+    return;
+  };
+
+  cotizar(moneda, criptomoneda);
+}
+
+
+
+// * Muestra una alerta en pantalla
+const mostrarAlerta = (mensaje) => {
+  const exiteAlerta = document.querySelector('.error');
+
+  if (!exiteAlerta) {
+    const alertaDiv = document.createElement('DIV');
+    alertaDiv.textContent = mensaje;
+    alertaDiv.classList.add('error');
+
+    formulario.appendChild(alertaDiv);
+
+    // Eliminamos la alerta
+    setTimeout(() => {
+      alertaDiv.remove();
+    }, 2000);
+  }
+};
+
+
+
+// * Cotiza una criptomoneda
+const cotizar = (moneda, criptomoneda) => {
+  const URL = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+  mostrarSpinner();
+
+  fetch(URL)
+    .then(respuesta => respuesta.json())
+    .then(resultado => {
+      const { DISPLAY } = resultado;
+      const cotizacion = DISPLAY[criptomoneda][moneda]
+      mostrarCotizacion(cotizacion);
+    });
+};
+
+
+
+// * Muestra la cotizacion en pantalla
+const mostrarCotizacion = (cotizacion) => {
+
+  limpiarHtml();
+
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+  const precio = document.createElement('P');
+  precio.classList.add('precio');
+  precio.innerHTML = ` El precio es: <span>${PRICE}</span> `;
+
+  const precioAlto = document.createElement('P');
+  precioAlto.innerHTML = `Precio mas alto del dia <span>${HIGHDAY}</span> `;
+
+  const precioBajo = document.createElement('P');
+  precioBajo.innerHTML = `Precio mas bajo del dia <span>${LOWDAY}</span> `;
+
+  const ultimasHoras = document.createElement('P');
+  ultimasHoras.innerHTML = `Variación ultimas 24 Horas <span>${CHANGEPCT24HOUR}%</span> `;
+
+  const ultimaActualizacion = document.createElement('P');
+  ultimaActualizacion.innerHTML = `Ultima actualización <span>${LASTUPDATE}</span> `;
+
+  divResultado.append(precio, precioAlto, precioBajo, ultimasHoras, ultimaActualizacion);
+};
+
+
+
+// * Muestra spinner
+const mostrarSpinner = () => {
+
+  limpiarHtml();
+
+  const spinner = document.createElement('DIV');
+  spinner.classList.add('spinner');
+  spinner.innerHTML = `
+    <div class="bounce1"></div>
+    <div class="bounce2"></div>
+    <div class="bounce3"></div>
+  `;
+
+  divResultado.appendChild(spinner);
+};
+
+
+// * Limpia el html previo 
+const limpiarHtml = () => {
+  while (divResultado.firstChild) {
+    divResultado.firstChild.remove();
+  }
+};
+
+
+
+// * EVENTOS
+// * Cuando el documento esta listo
+document.addEventListener('DOMContentLoaded', () => {
+  obtenerCriptomonedas();
+  formulario.addEventListener('submit', cotizarCriptomoneda);
+});
+
